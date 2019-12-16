e6459e5089f8e2f9fa7d49d47d7a02a483f93af3
Chakrapani Gautam <cpgautam1998@gmail.com>
2019-12-16 19:28:41 +0530

    Cleanup! 16/12/2019

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 let express     = require( 'express' );
 let serv_app    = express();
 let path        = require( 'path' );
+let http        = require( 'http' ).createServer( serv_app );
+let io          = require( 'socket.io' )( http );
 
 serv_app.get( '/', function( req, res ) {
     let abs_path = path.join( __dirname, 'public/index.html' );
     res.sendFile( abs_path );
 });
 
+io.on( 'connection', function( socket ) {
+    console.log( "a user connected" ); 
+   
+    socket.on( 'disconnect', function() {
+        console.log( "user disconnect" );
+    });
+});
+
 module.exports = {
     start: function() {
-        serv_app.listen( 8080 );
+        http.listen( 3030, function() {
+            console.log( "Listening to *3030" );
+        });
     }
 }
